0d3f35289e905b45e9a1937400f8e2e9a8455fda
Chinchutha555 <Chinchutha.th@mail.kmutt.ac.th>
2021-08-27 00:36:01 +0700

    update tocel

--- tocel.js
+++ tocel.js
@@ -1,1 +1,7 @@
-console.log("hello")
+function tocelsius(){
+    let fahrenheit = 40
+    let value = (fahrenheit-32) * 5/9 
+    // document.getElementById("resultss").innerHTML = value.toFixed(2)+" cํ"
+    return value
+}
+console.log(tocelsius().toFixed(2))
