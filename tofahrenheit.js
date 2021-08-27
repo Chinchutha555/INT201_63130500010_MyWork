6eadfe04b5528081aaa0d5b476c84f97e09793b7
Chinchutha555 <Chinchutha.th@mail.kmutt.ac.th>
2021-08-27 23:39:54 +0700

    update to fah

--- tofahrenheit.js
+++ tofahrenheit.js
@@ -1,8 +1,8 @@
 // ทดสอบเปลี่ยนองศาเซลเซียส เป็น ฟาเรนไฮต์ 
-function tofah(){
-    let oc = 30
+function tofah(oc){
+    // let oc = 30
     let value = (oc*(9/5)+32)
     // document.getElementById("resultss").innerHTML = value
     return value
 }
-console.log(tofah())
+console.log(tofah(30))
